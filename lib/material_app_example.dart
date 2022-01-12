5c1dc7e4d1bb77aa2558e269ec60570f0678d077
Md Faisal M <faisalchanglah@gmail.com>
2022-01-12 14:36:09 +0500

    flutter_course

--- lib/material_app_example.dart
+++ lib/material_app_example.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  const message = 'Allah-Ho-Akbar';
+  const message = 'Allah-Hoooo-Akbar';
   runApp(
     MaterialApp(
       home: Center(
