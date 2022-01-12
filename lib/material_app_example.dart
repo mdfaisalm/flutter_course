c539de40d87e3a2b7eb5d822cab522a303ed6b7d
Md Faisal M <faisalchanglah@gmail.com>
2022-01-12 11:28:23 +0500

    flutter_course

---
+++ lib/material_app_example.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  const message = 'Allah-Ho-Akbar';
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text(
+          message,
+        ),
+      ),
+    ),
+  );
+}
---
+++ lib/scaffold_example.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue[900],
+        body: Center(
+          child: Image(
+            width: 200,
+            height: 200,
+            image: AssetImage(
+              "a/a.png",
+            ),
+          )
+          /*          child: Image(
+            width: 200,
+            height: 200,
+            image: NetworkImage(
+              "https://www.computerhope.com/jargon/a/android.png",
+            ),
+          )*/
+          ,
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.amberAccent[100],
+          title: Text(
+            "Allah",
+          ),
+        ),
+      ),
+    ),
+  );
+}
