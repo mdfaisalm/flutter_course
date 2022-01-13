1364f569d15fcf24ec4624021717eecbd3e8dc04
Md Faisal M <faisalchanglah@gmail.com>
2022-01-13 18:11:53 +0500

    flutter_course

--- lib/run_file.dart
+++ lib/run_file.dart
@@ -14,36 +14,8 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                height: double.infinity,
-                width: 100,
-                color: Color(0xfff44133),
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Color(0xffFFEC38),
-                  ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Color(0xff4BB14F),
-                  ),
-                ],
-              ),
-              Container(
-                height: double.infinity,
-                width: 100,
-                color: Color(0xff1B97F3),
-              ),
-            ],
+          child: Column(
+            children: [CircleAvatar()],
           ),
         ),
       ),
