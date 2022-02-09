228f8772c18da53aafd3d8756055073dbfa949f4
FRandyDevs <faisalchanglah@gmail.com>
2022-02-09 22:07:50 +0500

    flutter_26_1_22

--- lib/run_file.dart
+++ lib/run_file.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_course/story_bank.dart';
 
@@ -25,86 +27,47 @@ class _StoryPageState extends State<StoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        //TODO: Step 1 - Add background.png to this Container as a background image.
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage(
-                "images/background.png",
-              ),
-              fit: BoxFit.cover),
-        ),
-        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
-        constraints: BoxConstraints.expand(),
-        child: SafeArea(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
+            children: [
               Expanded(
-                flex: 12,
-                child: Center(
-                  child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    storyBrain.getStory(),
-                    style: TextStyle(
-                      fontSize: 25.0,
-                    ),
-                  ),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Container(
+                      color: Colors.blue,
+                      // child: Text("ABC"),
+                    )),
+                    Expanded(
+                        child: Container(
+                      color: Colors.red,
+                    ))
+                  ],
                 ),
               ),
               Expanded(
-                flex: 2,
-                child: FlatButton(
-                  onPressed: () {
-                    //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
-                    setState(() {
-                      storyBrain.nextStory(1);
-                    });
-                  },
-                  color: Colors.red,
-                  child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    storyBrain.getChoice1(),
-                    style: TextStyle(
-                      fontSize: 20.0,
-                    ),
-                  ),
+                child: Container(
+                  color: Colors.purple,
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
               ),
               Expanded(
-                flex: 2,
-                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
-                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: Visibility(
-                  visible: storyBrain.buttonShouldBeVisible(),
-                  child: FlatButton(
-                    onPressed: () {
-                      //Choice 2 made by user.
-                      //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
-                      setState(() {
-                        storyBrain.nextStory(2);
-                      });
-                    },
-                    color: Colors.blue,
-                    child: Text(
-                      //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                      storyBrain.getChoice2(),
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Container(
+                      color: Colors.orange,
+                    )),
+                    Expanded(
+                        child: Container(
+                      color: Colors.green,
+                    ))
+                  ],
                 ),
-              ),
+              )
             ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
 
